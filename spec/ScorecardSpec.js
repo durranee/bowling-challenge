61398cd20d7c69e9e0507e4ea896af8049781118
durranee <durranee@hotmail.com>
2018-03-25 04:16:44 +0100

    adding roll to current frame

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -5,16 +5,20 @@ describe("Scorecard", function() {
     scorecard = new Scorecard();
   });
 
+  it("Should start with empty score board", function(){
+    expect (scorecard.getScoreBoard()).toEqual([]);
+  });
+
   it("Should return total score", function(){
     expect (scorecard.getTotalScore()).toEqual(0);
   });
 
   it("should return the current frame number", function(){
-    expect(scorecard.getCurrentFrameNumber()).toEqual(1);
+    expect(scorecard.getCurrentFrameNumber()).toEqual(0);
   });
 
   it("should return current roll number", function() {
-    expect(scorecard.getCurrentRollNumber()).toEqual(1);
+    expect(scorecard.getCurrentRollNumber()).toEqual(0);
   });
 
   it("Should add to total score", function(){
@@ -24,25 +28,25 @@ describe("Scorecard", function() {
 
   it("should increase roll number by 1", function(){
     scorecard.roll(5);
-    expect(scorecard.getCurrentRollNumber()).toEqual(2);
+    expect(scorecard.getCurrentRollNumber()).toEqual(1);
   });
 
-  it("should reset roll number back to 1", function(){
+  it("should reset roll number back to 0", function(){
     scorecard.roll(10);
     scorecard.resetRollNumber();
-    expect(scorecard.getCurrentRollNumber()).toEqual(1);
+    expect(scorecard.getCurrentRollNumber()).toEqual(0);
   });
 
   it("should increase frame number by 1 after 2 rolls", function(){
     scorecard.roll(1);
     scorecard.roll(1);
-    expect(scorecard.getCurrentFrameNumber()).toEqual(2);
+    expect(scorecard.getCurrentFrameNumber()).toEqual(1);
   });
 
   it("shoud not increase frame number after 10 frames", function(){
     for (var i = 0; i < 30; i++)
       scorecard.roll(1);
-    expect(scorecard.getCurrentFrameNumber()).toEqual(10);
+    expect(scorecard.getCurrentFrameNumber()).toEqual(9);
   });
 
   it("should return true if there's a strike", function(){
@@ -66,6 +70,22 @@ describe("Scorecard", function() {
   it("should return false if no spare", function(){
     scorecard.roll(5)
     expect(scorecard.isSpare(4)).toEqual(false);
+  });
+
+  it("should reset pins standing after every frame", function(){
+    scorecard.roll(2);
+    scorecard.roll(3);
+    expect(scorecard.getPinsStanding()).toEqual(10);
+  });
+
+  it("should push current roll to current frame", function(){
+    scorecard.pushRolltoFrame(2);
+    expect(scorecard.getCurrentFrame()).toContain(2);
+  });
+
+  it("should push frame score card to scoreboard", function(){
+    scorecard.pushFrametoScoreBoard([2, 5]);
+    expect(scorecard.getScoreBoard()).toContain([2, 5]);
   });
 
 });
--- src/Scorecard.js
+++ src/Scorecard.js
@@ -1,11 +1,19 @@
 function Scorecard() {
-  const _MAXFRAMES = 10;
+  const _MAXFRAMES = 9; // (0-9) 10 frames for array
+  const _ALL_PINS = 10;
+  this._scoreBoard = [];
+  this._currentFrame = [];
   this._totalScore = 0;
-  this._currentFrameNumber = 1;
-  this._currentRollNumber = 1;
-  this._pinsStanding = 10;
+  this._currentFrameNumber = 0; // starting from 0... 0-9 (10 frames)
+  this._currentRollNumber = 0; // starting from 0 ... 0-1 (2 rolls)
+  this._pinsStanding = _ALL_PINS;
 
 /////////////////////////// GETTERS
+
+  Scorecard.prototype.getScoreBoard = function () {
+    return this._scoreBoard;
+  };
+
   Scorecard.prototype.getTotalScore = function () {
     return this._totalScore;
   };
@@ -23,39 +31,58 @@ function Scorecard() {
     return this._pinsStanding;
   };
 
-
+  Scorecard.prototype.getCurrentFrame = function () {
+    return this._currentFrame;
+  };
 /////////////////////////// QUERIES
 
   Scorecard.prototype.isStrike = function (pinsDropped) {
-    return (this._pinsStanding - pinsDropped  === 0 && this.getCurrentRollNumber() == 1)
+    return (this._pinsStanding - pinsDropped  === 0 && this.getCurrentRollNumber() == 0)
   };
 
   Scorecard.prototype.isSpare = function (pinsDropped) {
-    return (this._pinsStanding - pinsDropped  === 0 && this.getCurrentRollNumber() == 2)
+    return (this._pinsStanding - pinsDropped  === 0 && this.getCurrentRollNumber() == 1)
   };
 
 
 /////////////////////////// SETTERS
 
+  Scorecard.prototype.pushFrametoScoreBoard = function (frame) {
+    this._scoreBoard.push(frame);
+  };
+
+  Scorecard.prototype.pushRolltoFrame = function (pinsDropped) {
+    this._currentFrame.push(pinsDropped);
+  };
+
   Scorecard.prototype.addToTotal = function (pinsDropped) {
     this._totalScore = pinsDropped;
   };
 
+  Scorecard.prototype.resetPinsStanding = function () {
+    this._pinsStanding = _ALL_PINS;
+  };
+
+  Scorecard.prototype.resetRollNumber = function () {
+    this._currentRollNumber = 0;
+  };
+
   Scorecard.prototype.roll = function (pinsDropped) {
-    if (this.getCurrentRollNumber() + 1 > 2 && this._currentFrameNumber < _MAXFRAMES) {
+      // second roll of the frame
+    if (this.getCurrentRollNumber() + 1 > 1 && this._currentFrameNumber < _MAXFRAMES) {
       this._pinsStanding -= pinsDropped;
-      this.resetRollNumber
+      this.resetRollNumber();
       this._currentFrameNumber++;
+      this.resetPinsStanding();
     }
     else {
+      // first roll of the frame
       this._pinsStanding -= pinsDropped;
       this._currentRollNumber++;
     }
   };
 
-  Scorecard.prototype.resetRollNumber = function () {
-    this._currentRollNumber = 1;
-  };
+
 
 
 
